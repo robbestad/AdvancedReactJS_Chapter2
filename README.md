926ffab8390874ca77a1e4057149f5adb28e3d48
Sven Anders Robbestad <robbestad@gmail.com>
2017-04-10 15:32:06 +0200

    Add redux_02

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Example 1
 cd redux_01
 yarn && yarn start
 ´´´
+
+Example 2
+´´´
+cd redux_02
+yarn && yarn start
+´´´
+
+
+
---
+++ redux_02/index.js
@@ -0,0 +1,19 @@
+const createStore = require('redux').createStore;
+
+const reducer = (state = {count: 0}, action) => {
+  switch (action.type) {
+    case "INCREMENT": 
+      return {...state, count: state.count + action.payload}
+    default: 
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()))
+
+setInterval(() => store.dispatch({ type: 'INCREMENT', payload: 1 }), 1000);
+
+console.log("GO!")
+
